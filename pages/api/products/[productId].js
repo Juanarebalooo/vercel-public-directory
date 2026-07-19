c366924ea81b3c34c6db8f092a2068d881c6c3f3
Juanarebalooo <juanarebalooo@gmail.com>
2026-07-19 00:09:03 -0300

    cuarto commit

--- pages/api/products/[productId].js
+++ pages/api/products/[productId].js
@@ -2,7 +2,7 @@ export default function handler(req, res) {
   res.status(200).json({
     body: req.body,
     query: req.query,
-    cookies: req.cookies,
+
     product: true,
   });
 }
--- pages/api/test.js
+++ pages/api/test.js
@@ -2,7 +2,6 @@ export default function handler(req, res) {
   res.status(200).json({
     body: req.body,
     query: req.query,
-    cookies: req.cookies,
     test: true,
   });
 }
